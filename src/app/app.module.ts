3edb100f983253581d7de34619c4301abe10a953
MarianaButnari <mbutnari@sapec.md>
2023-10-09 14:57:42 +0300

    enh(ui): add modernize template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// icons
+import { TablerIconsModule } from 'angular-tabler-icons';
+import * as TablerIcons from 'angular-tabler-icons/icons';
+
+//Import all material modules
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//Import Layouts
+import { FullComponent } from './layouts/full/full.component';
+import { BlankComponent } from './layouts/blank/blank.component';
+
+// Vertical Layout
+import { SidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { HeaderComponent } from './layouts/full/header/header.component';
+import { BrandingComponent } from './layouts/full/sidebar/branding.component';
+import { AppNavItemComponent } from './layouts/full/sidebar/nav-item/nav-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullComponent,
+    BlankComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BrandingComponent,
+    AppNavItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    TablerIconsModule.pick(TablerIcons),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [TablerIconsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
